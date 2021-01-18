6d1c760522813a2accbc29ae5a0bd51aafb319a6
ROMANO Cyril <romano.cyril@gmail.com>
2021-01-18 11:26:46 +0100

    Ex02

---
+++ src/org/cro/jrpg/mundus/Monde.java
@@ -0,0 +1,32 @@
+package org.cro.jrpg.mundus;
+
+import java.util.Scanner;
+
+import org.cro.jrpg.personnage.Personnage;
+
+public class Monde {
+
+	/**
+	* Créer un personnage avec tous ses attributs (10pv, 1dmg)
+	* Demande a l'utilisateur d'entrer le nom du personnage.
+	* retour: une instance de la classe Personnage correctement instancié.
+	**/
+	public static Personnage personnageFactory() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Choisisez un nom :");
+			
+		String nom = sc.next();
+		
+		return new Personnage(10, 1, nom);	
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public static void afficherInformations() {
+		//TODO: 
+	}
+	
+}
